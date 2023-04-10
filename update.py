b2a1279bbb4a18aa9574658f1d0fc033be9497a8
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-04-10 13:48:40 +0530

    Update update.py

--- update.py
+++ update.py
@@ -4,12 +4,9 @@ from os import path as opath, environ as env
 from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
 from logging.handlers import RotatingFileHandler
 from subprocess import run as srun
-from dotenv import load_dotenv
 
 basicConfig(
     level=INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
-    datefmt="%d-%b-%y %I:%M:%S %p",
     handlers=[
         RotatingFileHandler("log.txt", maxBytes=50000000, backupCount=10),
         StreamHandler(),
@@ -33,6 +30,6 @@ if UPSTREAM_REPO is not None:
                      && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
 
     if update.returncode == 0:
-        log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+        log_info(f'Successfully updated with latest commit from {UPSTREAM_REPO}')
     else:
-        log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+        log_error(f'Something went wrong while updating, check {UPSTREAM_REPO} if valid or not!')
