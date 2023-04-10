f7f66f47841d2789511595eaf40e33049b9cee20
Silent Demon <105407900+SilentDemonSD@users.noreply.github.com>
2023-04-10 10:57:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'wzml'
-
+def check_up():
+    return 'Mkv-TBot Running!'
 
 if __name__ == "__main__":
     app.run()
